b203afd45c3586fa3c78e2224a6fb1abaa1878fb
Konrad Gerlach <konrad.gerlach_dev@gmx.de>
2022-10-26 19:41:05 +0200

    solved issue 6

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -17,5 +17,13 @@ describe "Author model", type: :model do
     author = Author.new(first_name: first_name,last_name: last_name,homepage: homepage)
     expect(author.name).to eq("Weid Al Jankovic")
   end
+  it "should validate the existence of last_name" do
+    author = Author.new(first_name: "whatever",homepage: "rocks your boat.com")
+    expect(author).to_not be_valid
+    author2 = Author.new(first_name: "whatever2",last_name: "",homepage: "rocks your boat.com")
+    expect(author2).to_not be_valid
+    author3 = Author.new(first_name: "whatever2",last_name: "wefgr",homepage: "rocks your boat.com")
+    expect(author3).to be_valid
+  end
 
 end
