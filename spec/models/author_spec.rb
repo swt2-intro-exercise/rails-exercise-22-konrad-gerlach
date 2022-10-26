2b0308b4d380df4a51240b88c08fb05e76339865
Konrad Gerlach <konrad.gerlach_dev@gmx.de>
2022-10-26 18:35:19 +0200

    implemented issue 4

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -10,5 +10,12 @@ describe "Author model", type: :model do
     expect(author.last_name).to eq(last_name)
     expect(author.homepage).to eq(homepage)
   end
+  it "should furthermore have a method name (no parameters) which returns the full name of an author (the first name followed by the last name)" do
+    first_name = "Weid Al"
+    last_name = "Jankovic"
+    homepage = "weird.com"
+    author = Author.new(first_name: first_name,last_name: last_name,homepage: homepage)
+    expect(author.name).to eq("Weid Al Jankovic")
+  end
 
 end
