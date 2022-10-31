a2a8e0ab801a4f96f155a5426dd4601a39f036df
Konrad Gerlach <konrad.gerlach_dev@gmx.de>
2022-10-31 18:07:30 +0100

    added author index page should link to edit author page

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -17,7 +17,17 @@ describe "Author Index page", type: :feature do
     expect(page).to have_xpath("//table/tr/td[text()='#{author.homepage}']")
   end
   it "should link to individual author's detail pages" do
+    Author.new(last_name:"wertfd").save()
+    visit authors_path
+    expect(page).to have_link 'Show', href: author_path(1)
+  end
+  it "should link to the new author page" do
     visit authors_path
     expect(page).to have_link 'New', href: new_author_path
+  end
+  it "should link to individual author's edit pages" do
+    Author.new(last_name:"wertfd").save()
+    visit authors_path
+    expect(page).to have_link 'Edit', href: edit_author_path(1)
   end
 end
