2dce1d97155e3ebd789abcfd4e7ef341b2f2369c
Konrad Gerlach <konrad.gerlach_dev@gmx.de>
2022-10-31 19:35:51 +0100

    added paper should not be valid without title

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
-RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "Paper model", type: :model do
+  it "should have title, venue and year instance variables" do
+    title = "Weid Al"
+    venue = "Jankovic"
+    year = 2002
+    paper = Paper.new(title: title, year: year, venue:venue)
+    expect(paper.title).to eq(title)
+    expect(paper.venue).to eq(venue)
+    expect(paper.year).to eq(year)
+  end
+  it "should validate the existence of title" do
+    paper = Paper.new(title: "", year: 2002, venue:"home")
+    expect(paper).to_not be_valid
+    paper = Paper.new(year: 2002, venue:"home")
+    expect(paper).to_not be_valid
+    paper = Paper.new(title: "some title",year: 2002, venue:"home")
+    expect(paper).to be_valid
+  end
+  it "should validate the existence of venue" do
+    paper = Paper.new(title: "some title", year: 2002, venue:"")
+    expect(paper).to_not be_valid
+    paper = Paper.new(title: "some title",year: 2002)
+    expect(paper).to_not be_valid
+    paper = Paper.new(title: "some title",year: 2002, venue:"home")
+    expect(paper).to be_valid
+  end
+  it "should validate the existence of year" do
+    paper = Paper.new(title: "some title", venue:"home")
+    expect(paper).to_not be_valid
+    paper = Paper.new(title: "some title",year: 2002, venue:"home")
+    expect(paper).to be_valid
+  end
+
 end
